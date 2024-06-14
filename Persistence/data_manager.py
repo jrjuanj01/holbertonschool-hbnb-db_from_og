c738490956aeb9361eb900ffb8a9a376d8258403
jrjuanj01 <jrjuanj01@gmail.com>
2024-06-14 02:32:01 -0400

    Testing circular import solution

--- Persistence/data_manager.py
+++ Persistence/data_manager.py
@@ -1,54 +1,56 @@
 import json
-import os
+from Persistence.data_loader import load_data
+# import os
 
 DATA_FILE = "data.json"
 
 
-def load_data():
-    """Load data from data.json file"""
-    from Models.amenity import Amenity
-    from Models.city import City
-    from Models.user import User
-    from Models.place import Place
-    from Models.review import Review
-    if os.path.exists(DATA_FILE):
-        with open(DATA_FILE, "r", encoding="utf-8") as f:
-            loaded_data = json.load(f)
-            storage = {
-                "User": {},
-                "Place": {},
-                "Review": {},
-                "City": {},
-                "Country": {},
-                "Amenity": {}
-            }
-            objects = {}
+# def load_data():
+#     """Load data from data.json file"""
+    
+#     if os.path.exists(DATA_FILE):
+#         with open(DATA_FILE, "r", encoding="utf-8") as f:
+#             loaded_data = json.load(f)
+#             storage = {
+#                 "User": {},
+#                 "Place": {},
+#                 "Review": {},
+#                 "City": {},
+#                 "Country": {},
+#                 "Amenity": {}
+#             }
+#             objects = {}
 
-            for data_type, items in loaded_data.items():
-                for identifier, obj_data in items.items():
-                    if data_type == "User":
-                        obj = User.from_dict(obj_data)
-                    elif data_type == "Place":
-                        obj = Place.from_dict(obj_data)
-                    elif data_type == "Review":
-                        obj = Review.from_dict(obj_data)
-                    elif data_type == "City":
-                        obj = City.from_dict(obj_data)
-                    elif data_type == "Amenity":
-                        obj = Amenity.from_dict(obj_data)
+#             for data_type, items in loaded_data.items():
+#                 for identifier, obj_data in items.items():
+#                     if data_type == "User":
+#                         from Models.user import User
+#                         obj = User.from_dict(obj_data)
+#                     elif data_type == "Place":
+#                         from Models.place import Place
+#                         obj = Place.from_dict(obj_data)
+#                     elif data_type == "Review":
+#                         from Models.review import Review
+#                         obj = Review.from_dict(obj_data)
+#                     elif data_type == "City":
+#                         from Models.city import City
+#                         obj = City.from_dict(obj_data)
+#                     elif data_type == "Amenity":
+#                         from Models.amenity import Amenity
+#                         obj = Amenity.from_dict(obj_data)
 
-                    storage[data_type][identifier] = obj_data
-                    objects[identifier] = obj
+#                     storage[data_type][identifier] = obj_data
+#                     objects[identifier] = obj
 
-            return storage, objects
-    return {
-        "User": {},
-        "Place": {},
-        "Review": {},
-        "City": {},
-        "Country": {},
-        "Amenity": {}
-        }
+#             return storage, objects
+#     return {
+#         "User": {},
+#         "Place": {},
+#         "Review": {},
+#         "City": {},
+#         "Country": {},
+#         "Amenity": {}
+#         }
 
 
 def save_data(data):
@@ -59,7 +61,8 @@ def save_data(data):
 
 class DataManager():
     """Class to manage all Data and CRUD methods"""
-    storage, objects = load_data()
+    storage = load_data()
+    objects = load_data()
 
     @classmethod
     def save(cls, identifier, data_type, object):
